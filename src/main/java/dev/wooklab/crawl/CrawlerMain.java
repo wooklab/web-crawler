7aa005a016def921a42eedb4224d241d245a7013
inwook9 <inwook9@gmail.com>
2019-03-24 23:49:05 +0900

    WEB Crawler Project Init

---
+++ src/main/java/dev/wooklab/crawl/CrawlerMain.java
@@ -0,0 +1,13 @@
+package dev.wooklab.crawl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrawlerMain {
+    private static final Logger logger = LoggerFactory.getLogger(CrawlerMain.class);
+
+
+    public static void main(String[] args) {
+        logger.info("hello..!");
+    }
+}
